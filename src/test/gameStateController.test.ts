aaad06935ba54540e51b9567c05f01dbd8c9d8e8
DTasev <dimtasev@gmail.com>
2018-02-12 16:17:41 +0000

    Fixed test and changed test command for travis

--- src/test/gameStateController.test.ts
+++ src/test/gameStateController.test.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import { EventController } from "../ts/gameStateController";
+import { GameStateController } from "../ts/gameStateController";
 
 describe('Event Controller', () => {
     it('should change the game state', () => {
-        let controller = new EventController();
+        let controller = new GameStateController();
 
 
     });
