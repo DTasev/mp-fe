c39dc45829caaf90c44c4680620dc5d3f570f481
DTasev <dimtasev@gmail.com>
2018-02-13 17:25:50 +0000

    Added line-circle collision

--- src/test/tanksMath.test.ts
+++ src/test/tanksMath.test.ts
@@ -30,5 +30,103 @@ describe('Tanks Math', () => {
         expect(TanksMath.point.collide_circle(point, obj_coords, obj_width)).to.be.true;
         obj_width = 1;
         expect(TanksMath.point.collide_circle(point, obj_coords, obj_width)).to.be.false;
-    })
+    });
+    it('should calculate the closest point on a horizontal line', () => {
+        // test horizontal line
+        let start = new CartesianCoords(0, 0);
+        let end = new CartesianCoords(4, 0);
+        let point = new CartesianCoords(2, 2);
+        let res = TanksMath.line.closest_point(start, end, point);
+        expect(res.X).to.equal(2);
+        expect(res.Y).to.equal(0);
+
+        start = new CartesianCoords(0, 0);
+        end = new CartesianCoords(3, 0);
+        point = new CartesianCoords(2, 2);
+        res = TanksMath.line.closest_point(start, end, point);
+        expect(res.X).to.equal(2);
+        expect(res.Y).to.equal(0);
+
+        start = new CartesianCoords(0, 0);
+        end = new CartesianCoords(500, 0);
+        point = new CartesianCoords(250, 250);
+        res = TanksMath.line.closest_point(start, end, point);
+        expect(res.X).to.equal(250);
+        expect(res.Y).to.equal(0);
+    });
+    it('should calculate the closest point on a diagonal line, up to the right', () => {
+        // test diagonal line / pointing up and to the right
+        let start = new CartesianCoords(0, 0);
+        let end = new CartesianCoords(2, 2);
+        let point = new CartesianCoords(0, 2);
+        let res = TanksMath.line.closest_point(start, end, point);
+        expect(res.X).to.equal(1);
+        expect(res.Y).to.equal(1);
+        res = TanksMath.line.closest_point(end, start, point);
+        expect(res.X).to.equal(1);
+        expect(res.Y).to.equal(1);
+
+        start = new CartesianCoords(1, 1);
+        end = new CartesianCoords(3, 3);
+        point = new CartesianCoords(0, 2);
+        res = TanksMath.line.closest_point(start, end, point);
+        expect(res.X).to.equal(1);
+        expect(res.Y).to.equal(1);
+        res = TanksMath.line.closest_point(end, start, point);
+        expect(res.X).to.equal(1);
+        expect(res.Y).to.equal(1);
+    });
+    it('should calculate the closest point on a vertical line', () => {
+        // test vertical line
+        let start = new CartesianCoords(0, 0);
+        let end = new CartesianCoords(0, 2);
+        let point = new CartesianCoords(1, 1);
+        let res = TanksMath.line.closest_point(start, end, point);
+        expect(res.X).to.equal(0);
+        expect(res.Y).to.equal(1);
+        res = TanksMath.line.closest_point(end, start, point);
+        expect(res.X).to.equal(0);
+        expect(res.Y).to.equal(1);
+    });
+    it('should calculate the closest point on a diagonal line, up to the left', () => {
+        // test diagonal line \, up and to the left
+        let start = new CartesianCoords(0, 2);
+        let end = new CartesianCoords(2, 0);
+        let point = new CartesianCoords(2, 2);
+        let res = TanksMath.line.closest_point(start, end, point);
+        expect(res.X).to.equal(1);
+        expect(res.Y).to.equal(1);
+        res = TanksMath.line.closest_point(end, start, point);
+        expect(res.X).to.equal(1);
+        expect(res.Y).to.equal(1);
+
+        start = new CartesianCoords(1, 3);
+        end = new CartesianCoords(3, 1);
+        point = new CartesianCoords(3, 3);
+        res = TanksMath.line.closest_point(start, end, point);
+        expect(res.X).to.equal(2);
+        expect(res.Y).to.equal(2);
+        res = TanksMath.line.closest_point(end, start, point);
+        expect(res.X).to.equal(2);
+        expect(res.Y).to.equal(2);
+    });
+    it('should calculate the closest point on a line, when the point is on the line', () => {
+        // test diagonal line \, up and to the left
+        let start = new CartesianCoords(0, 2);
+        let end = new CartesianCoords(2, 4);
+        let point = new CartesianCoords(1, 3);
+        let res = TanksMath.line.closest_point(start, end, point);
+        expect(res.X).to.equal(1);
+        expect(res.Y).to.equal(3);
+        res = TanksMath.line.closest_point(end, start, point);
+        expect(res.X).to.equal(1);
+        expect(res.Y).to.equal(3);
+    });
+    it('should check collision of a line with a circle', () => {
+        let start = new CartesianCoords(0, 2);
+        let end = new CartesianCoords(2, 4);
+        let point = new CartesianCoords(2, 2);
+        let radius = 2;
+        expect(TanksMath.line.collide_circle(start, end, point, radius)).to.be.true;
+    });
 });
--- src/ts/tanksMath.ts
+++ src/ts/tanksMath.ts
@@ -16,12 +16,12 @@ class Point {
     /**
      * Calculate if the point collides with the circle.
      * @param point The coordinates of the point (user's click)
-     * @param object The coordinates of the object
-     * @param radius The radius of the object
+     * @param center The centre of the circle
+     * @param radius The radius of the circle
      * @returns true if there is collision, false otherwise
      */
-    collide_circle(point: CartesianCoords, object: CartesianCoords, radius: number): boolean {
-        const distance = this.dist2d(point, object);
+    collide_circle(point: CartesianCoords, center: CartesianCoords, radius: number): boolean {
+        const distance = this.dist2d(point, center);
         if (distance > radius) {
             return false;
         }
@@ -29,6 +29,43 @@ class Point {
     }
 }
 
+class Line {
+    /** Find the closest point on a line. The closest point to 
+     * 
+     * @param start Start point of the line
+     * @param end End point of the line
+     * @param point Point for which the closest point on the line will be found.
+     */
+    closest_point(start: CartesianCoords, end: CartesianCoords, point: CartesianCoords): CartesianCoords {
+        // turn the line ito equation of the form Ax + By = C
+        const A1 = end.Y - start.Y,
+            B1 = start.X - end.X;
+
+        // find the perpendicular lines to the initial line segment
+        const C1 = A1 * start.X + B1 * start.Y,
+            C2 = -B1 * point.X + A1 * point.Y;
+
+        // use Cramer's Rule to solve for the point of intersection
+        const det = A1 * A1 + B1 * B1;
+        const closest_point: CartesianCoords = new CartesianCoords();
+        if (det != 0) {
+            closest_point.X = (A1 * C1 - B1 * C2) / det;
+            closest_point.Y = (A1 * C2 + B1 * C1) / det;
+        } else { // the point is on the line already
+            closest_point.X = point.X;
+            closest_point.Y = point.Y;
+        }
+
+        return closest_point;
+    }
+    collide_circle(start: CartesianCoords, end: CartesianCoords, center: CartesianCoords, radius: number): boolean {
+        const closest_point = this.closest_point(start, end, center);
+
+        return TanksMath.point.collide_circle(closest_point, center, radius);
+    }
+}
+
 export class TanksMath {
     static point: Point = new Point();
+    static line: Line = new Line();
 }
