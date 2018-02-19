dae5aa48ac146eb9262f389a6150ca87c25f8ddb
DTasev <dimtasev@gmail.com>
2018-02-19 12:29:40 +0000

    Fixed tests

--- src/test/limiters/action.test.ts
+++ src/test/limiters/action.test.ts
@@ -8,8 +8,18 @@ describe('Action limiter', () => {
     const number_of_turns = 3;
     it('should signal when enough actions have been taken', () => {
         const turn = new Actions(number_of_turns);
-        expect(turn.end()).to.be.false;
-        expect(turn.end()).to.be.false;
-        expect(turn.end()).to.be.true;
+        turn.take();
+        expect(turn.over()).to.be.false;
+        turn.take();
+        expect(turn.over()).to.be.false;
+        turn.take();
+        expect(turn.over()).to.be.true;
     });
+    it('should be able to end the turn early', () => {
+        const turn = new Actions(number_of_turns);
+        turn.take();
+        expect(turn.over()).to.be.false;
+        turn.end();
+        expect(turn.over()).to.be.true;
+    })
 });
