314726cdee0798f330a14ead29eac31a616b96f7
DTasev <dimtasev@gmail.com>
2018-02-19 12:29:30 +0000

    Added json2html own library

--- src/ts/ui.ts
+++ src/ts/ui.ts
@@ -1,5 +1,10 @@
+import { J2H } from "./json2html";
+
 export class Ui {
-    div: HTMLDivElement;
+    private readonly div: HTMLDivElement;
+    private readonly left: HTMLDivElement;
+    private readonly right: HTMLDivElement;
+
     constructor(id: string, width: number) {
         this.div = <HTMLDivElement>document.getElementById(id);
         if (!this.div) {
@@ -7,6 +12,34 @@ export class Ui {
         }
 
         this.setWidth(width);
+
+        const left = {
+            "div": {
+                "className": "w3-col s11 m11 l11",
+                "children": [{
+                    "p": {
+                        "textContent": "a"
+                    }
+                }]
+            }
+        };
+        this.left = <HTMLDivElement>J2H.json2html(left);
+
+        const right = {
+            "div": {
+                "className": "w3-col s1 m1 l1",
+                "children": [{
+                    "button": {
+                        "style": "width:100%",
+                        "textContent": "Apples"
+                    }
+                }]
+            }
+        };
+        this.right = <HTMLDivElement>J2H.json2html(right);
+
+        this.div.appendChild(this.left);
+        this.div.appendChild(this.right);
     }
     setWidth(width: number) {
         // as any ignores the read-only "style" warning, as we need to write the width of the canvas to the width of the UI element
