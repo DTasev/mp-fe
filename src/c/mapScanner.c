088f69713c42598c92dbd8096a9c73f0c1d647e6
Dimitar Tasev <dimtasev@gmail.com>
2018-03-24 13:26:57 +0000

    Added comments to C code, and O3 build

--- src/c/mapScanner.c
+++ src/c/mapScanner.c
@@ -2,6 +2,8 @@
 
 #define DEBUG1
 
+const static int channels = 4;
+
 void myimage(unsigned char *const in, unsigned char *out, int width, int height) {
 #ifdef DEBUG
   printf("Inside C function: width: %d, height: %d\n", width, height);
@@ -15,7 +17,6 @@ void myimage(unsigned char *const in, unsigned char *out, int width, int height)
   // position index for the out array
   int posout = 0;
 
-  const int channels = 4;
   // the data has more than one channel per pixel, projected into a 1d array
   // the actual 'width' of the image would then be the normal width multiplied by the number of channels
   const int widthWithChannels = width * channels;
@@ -30,7 +31,13 @@ void myimage(unsigned char *const in, unsigned char *out, int width, int height)
 #endif
     // loop over every column
     for (col = 0, colout = 0; colout < width; col += channels, ++colout) {
+      // the input array is a RGBA image (4 channels) projected into a 1D array, total size: W * H * Channels
+      // this calculates the offset correctly, using the widthWithChannels to account for
+      // 4 (number of channels) time larger width. The height of the image is not changed
       pos = row * widthWithChannels + col;
+
+      // the out array is a 1 channel image, with the same width and height as the original,
+      // the total size is W * H * 1
       posout = row * width + colout;
 #ifdef DEBUG
       printf("in[%d] ", pos);
@@ -38,7 +45,7 @@ void myimage(unsigned char *const in, unsigned char *out, int width, int height)
              in[pos + 2], pos + 3, in[pos + 3]);
 #endif
 
-      // the alpha is not used in the grayscale calculation
+      // the alpha is not used in the grayscale calculation, therefore assuming the pixel is fully visible
       out[posout] = (in[pos] + in[pos + 1] + in[pos + 2]) / 3;
 #ifdef DEBUG
       printf("out[%d]: %d\n", posout, out[posout]);
