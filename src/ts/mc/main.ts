47c6db096f609823e39464e03010193d0a84d212
Dimitar Tasev <dimtasev@gmail.com>
2018-03-28 20:52:31 +0100

    Added River Crossing map

--- src/ts/mc/main.ts
+++ src/ts/mc/main.ts
@@ -10,8 +10,11 @@ let draw: Draw;
 
 let context: CanvasRenderingContext2D;
 let lineCache = new LineCache();
+let centers: Point[] = [];
 let line = new Line();
 const lineColor = Color.black().rgba();
+
+let obstacles = { "terrain": [] };
 
 function startDrawingMap(e: MouseEvent) {
     draw.updatePosition(e);
@@ -20,20 +23,15 @@ function startDrawingMap(e: MouseEvent) {
 
 function stopDrawingMap(e: MouseEvent) {
     draw.state = DrawState.STOPPED;
+    // close the obstacle by connecting the last to the first points. This is done automatically
+    // during Tanks' map drawing.
     Draw.line(context, line.points[line.points.length - 1], line.points[0], 3, lineColor);
+    // cache the current obstacle
     lineCache.lines.push(line.copy());
+    // reset the current points so that new obstacles can start separated
     line.points = [];
+    // clear the last mouse position
     draw.last = new Point();
-    const d = { "terrain": [] };
-    for (let i = 0; i < lineCache.lines.length - 1; i += 2) {
-        const obstacle = {};
-        const center = lineCache.lines[i + 1];
-        obstacle["centerX"] = center.points[0].x;
-        obstacle["centerY"] = center.points[0].y;
-        obstacle["points"] = lineCache.lines[i];
-        d.terrain.push(obstacle);
-    }
-    console.log(d);
 }
 
 function drawMap(e: MouseEvent) {
@@ -41,6 +39,49 @@ function drawMap(e: MouseEvent) {
         draw.updatePosition(e);
         draw.mouseLine(context, 3, lineColor);
         line.points.push(draw.mouse.copy());
+    }
+}
+
+function setCenter(e: MouseEvent) {
+    draw.updatePosition(e);
+    e.preventDefault();
+    Draw.circle(context, draw.mouse, 1, 1, Color.red().rgba());
+    // if there is equal amount of obstacles, then set the center for the last one
+    if (centers.length == lineCache.lines.length) {
+        centers[centers.length - 1] = draw.mouse.copy();
+    } else {
+        centers.push(draw.mouse.copy());
+    }
+
+    obstacles = { "terrain": [] };
+    for (let i = 0; i < lineCache.lines.length; ++i) {
+        const obstacle = {};
+        obstacle["centerX"] = centers[i].x;
+        obstacle["centerY"] = centers[i].y;
+        obstacle["points"] = lineCache.lines[i].points;
+        obstacles.terrain.push(obstacle);
+    }
+    console.log(obstacles);
+    console.log(JSON.stringify(obstacles));
+    console.log('Object `obstacles` globally accessible.');
+    window["obstacles"] = obstacles;
+}
+
+function mouseForward(e: MouseEvent, action_LMB: Function, action_MMB?: Function, action_RMB?: Function) {
+    switch (e.button) {
+        case 0: // LMB
+            if (action_LMB)
+                action_LMB(e);
+            break;
+        case 1: // MMB
+            if (action_MMB)
+                action_MMB(e);
+            break;
+        case 2: // RMB
+            if (action_RMB)
+                action_RMB(e);
+        default:
+            throw new Error("Button does nothing");
     }
 }
 
@@ -52,13 +93,14 @@ function init() {
     const canvasHeight = 1024;
 
     const canvas = <HTMLCanvasElement>document.getElementById(Settings.ID_GAME_CANVAS);
+    canvas.style.border = "1px solid";
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
     context = canvas.getContext("2d");
 
-    canvas.onmousedown = startDrawingMap;
-    canvas.onmouseup = stopDrawingMap;
-    canvas.onmousemove = drawMap;
+    canvas.onmousedown = (e: MouseEvent) => mouseForward(e, startDrawingMap, setCenter);
+    canvas.onmouseup = (e: MouseEvent) => mouseForward(e, stopDrawingMap);
+    canvas.onmousemove = (e: MouseEvent) => mouseForward(e, drawMap);
 }
 
 init();
