bd46fdc8bf67d1c54bad21ef8e5478732388a79c
Dimitar Tasev <dimtasev@gmail.com>
2018-03-29 14:37:30 +0100

    Always subtract scrollbars if not mobile

--- src/ts/tanks/main.ts
+++ src/ts/tanks/main.ts
@@ -22,12 +22,13 @@ const SCROLLBAR_WIDTH = 17;
 // Set-up the canvas and add our event handlers after the page has loaded
 function init() {
     console.log(Settings.IS_MOBILE ? 'Running on mobile' : 'Running on PC');
+    // const hasVerticalScroll = window.innerWidth > document.documentElement.clientWidth;
+    // console.log("Window vertical scroll:", hasVerticalScroll);
+
     // subtracting the scrollbar width prevents unlimited X scrolling to the right
-    const hasVerticalScroll = window.innerWidth > document.documentElement.clientWidth;
-    console.log("Window vertical scroll:", hasVerticalScroll);
-    // Don't subtract if there is no scrollbar
-    const viewportWidth = hasVerticalScroll ? window.innerWidth - SCROLLBAR_WIDTH : window.innerWidth;
-    const viewportHeight = hasVerticalScroll ? window.innerHeight - SCROLLBAR_WIDTH : window.innerHeight;
+    // Don't subtract scrollbar from mobile
+    const viewportWidth = Settings.IS_MOBILE ? window.innerWidth : window.innerWidth - SCROLLBAR_WIDTH;
+    const viewportHeight = Settings.IS_MOBILE ? window.innerHeight : window.innerHeight - SCROLLBAR_WIDTH;
 
     const ui = new Ui(Ui.ID_GAME_UI, viewportWidth, viewportHeight);
     const canvas = <HTMLCanvasElement>document.getElementById(Settings.ID_GAME_CANVAS);
