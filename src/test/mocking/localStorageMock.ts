fd2a60d17c0fe4cb78f2f8aeb75c83cbd0e775a0
Dimitar Tasev <dimtasev@gmail.com>
2018-04-09 15:08:29 +0100

    Improved localStorage mock, fixed getItem, added clear

--- src/test/mocking/localStorageMock.ts
+++ src/test/mocking/localStorageMock.ts
@@ -1,24 +1,32 @@
 export class LocalStorageMock implements Storage {
-    dict = {
-        "map1": '{"ready":true,"name":"Swamp","creator":1,"created":"2018-03-30T13:52:25.302672Z","terrain":[{"id":0,"type":"water","center":{"x":1234.423076923077,"y":525.2307692307693},"points":[{"x":1191,"y":168}]}],"cached":1522687850211}'
-    };
+    storage = {};
 
     [key: string]: any;
     length: number;
+    constructor() {
+        this.storage["map1"] = '{"ready":true,"name":"Swamp","creator":1,"created":"2018-03-30T13:52:25.302672Z","terrain":[{"id":0,"type":"water","center":{"x":1234.423076923077,"y":525.2307692307693},"points":[{"x":1191,"y":168}]}],"cached":1522687850211}'
+    }
+
     clear(): void {
-        throw new Error("Method not implemented.");
+        this.storage = {};
     }
+
     getItem(key: string): string {
-        const d = this.dict[key];
-        return d;
+        if (this.storage.hasOwnProperty(key)) {
+            return this.storage[key];
+        }
+        return null;
     }
+
     key(index: number): string {
         throw new Error("Method not implemented.");
     }
+
     removeItem(key: string): void {
         throw new Error("Method not implemented.");
     }
+
     setItem(key: string, value: string): void {
-        throw new Error("Method not implemented.");
+        this.storage[key] = value;
     }
 }
